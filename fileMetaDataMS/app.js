25101f4898863e12ccbaaf17c8578ff1aa6e8df7
rehas <beratson@hotmail.com>
2017-12-18 21:01:30 +0300

    robots favicon handling

--- fileMetaDataMS/app.js
+++ fileMetaDataMS/app.js
@@ -8,6 +8,14 @@ app.use('/', function (req, res) {
     res.send(req.headers);
 })
 
+app.get('/favicon.ico', function (req, res) {
+    res.send("Hello World");
+})
+
+app.get('/robots.txt', function (req, res) {
+    res.send("Hello World");
+})
+
 app.listen(port, function() {
 console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 
