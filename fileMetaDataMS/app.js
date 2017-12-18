173102e60b2db7e593beaad7e72fa30933a651f0
rehas <beratson@hotmail.com>
2017-12-18 20:59:58 +0300

    testing heroku

--- fileMetaDataMS/app.js
+++ fileMetaDataMS/app.js
@@ -1,3 +1,14 @@
 ﻿var port = process.env.port;
-
+var express = require('express');
 console.log(port);
+
+var app = express();
+
+app.use('/', function (req, res) {
+    res.send(req.headers);
+})
+
+app.listen(port, function() {
+console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
+});
