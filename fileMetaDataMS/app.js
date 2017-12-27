d8f9b75caefd7709f5a8662eae3bdb5fabf07454
rehas <beratson@hotmail.com>
2017-12-27 18:24:11 +0300

    Backend file upload handled, project completed without styling

--- fileMetaDataMS/app.js
+++ fileMetaDataMS/app.js
@@ -1,11 +1,24 @@
 ﻿var express = require('express');
 var multer = require('multer');
+//var upload = multer({ dest : 'uploads/' });   
 var path = require('path');
 var port = process.env.PORT || 1350;
 console.log(port);
 
 
 var app = express();
+
+app.use(express.static('uploads'));
+
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads')
+    },
+    filename: function (req, file, callback) {
+        console.log(file)
+        callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
 
 app.use('/', express.static(__dirname + '/public'));
 
@@ -17,11 +30,39 @@ app.get('/favicon.ico', function (req, res) {
     res.end("<h1>Hello World</h1>");
 })
 
+var response = {
+    size : 0,
+};
+app.post('/api/file/', function (req, res) {
+    console.log("file handling");
+    var upload = multer({
+        storage : storage
+    }).single('uFile');
+    upload(req, res, function (err) {
+        //res.end('File is uploaded');
+        console.log(req.body);
+        console.log("File is " + JSON.stringify(req.file));
+        console.log("File size is  " + req.file.size);
+        response["size"] = req.file.size;
+
+        console.log("Response is " + JSON.stringify(response));
+    })
+    
+    console.log("Response is " + JSON.stringify(response));
+
+    res.redirect('/result');
+    //console.log("Multer File is  " + multerFile);
+});
+
+app.get('/result', function (req, res) {
+    res.send(response);
+});
+
 app.get('/robots.txt', function (req, res) {
     res.end("<h1>Hello World</h1>");
 })
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 
 });
--- fileMetaDataMS/public/index.js
+++ fileMetaDataMS/public/index.js
@@ -1,5 +1,45 @@
 ﻿var myApp = angular.module('myApp', []);
 
-myApp.controller('myController', function ($scope) {
-    $scope.testVar = 5;
-});
+myApp.controller('myController', ['$scope', '$http', function ($scope, $http) {
+        $scope.testVar = 5;
+        
+        $('#uFile').on('change', function () {
+            console.log("File upload changed");
+            var formData = new FormData();
+            
+            var filesU = $(this).get(0).files;
+            
+            if (filesU.length > 0) {
+                
+                formData.append('uFile', filesU[0], filesU[0].name);
+            }
+        });
+        
+        $scope.uploadFile = function () {
+            console.log("File taken");
+            
+            $scope.ufile = document.getElementById('fileUpload').files;
+            
+            
+            //$scope.uFile = element.files;
+            
+            
+            //alert("Your File Size is" + file[0].files[0].size);
+            
+            $http({
+                method : 'POST',
+                url : '/api/file/',
+                
+                data: {
+                    upload : formData,
+                },
+            });
+        }
+        
+        $scope.fileChange = function (elem) {
+            
+            console.log("Elem from fileChange is" + elem);
+            $scope.uFile = elem;
+        }
+    }]);
+
